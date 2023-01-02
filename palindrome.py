252dedf74f1a8b059bcb2e7f428dfc957f3ad541
KaterynaManko <k.manko16@gmail.com>
2023-01-02 18:17:01 +0200

    Add corrections according to comments

--- palindrome.py
+++ palindrome.py
@@ -1,15 +1,10 @@
 def palindrome(word):
     """Checks whether a word is a palindrome"""
-    letters_list = [letter for letter in word]
-    reversed_letters_list = letters_list[::-1]
-    if(letters_list==reversed_letters_list):
-     return True
+    reversed_word = word[::-1]
+    if (word == reversed_word):
+     print("Паліндром")
     else:
-     return False
+     print("Непаліндром")   
 
-word = "око"
-word_verification = palindrome(word)
-if word_verification:
-    print("Паліндром")
-else:
-    print("Непаліндром")
+word = "анна"
+palindrome(word)
