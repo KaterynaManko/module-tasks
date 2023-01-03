ee653974be9aa0ecdcdb8b5fda198ea6991a03ad
KaterynaManko <k.manko16@gmail.com>
2023-01-03 22:30:09 +0200

    Add corections

--- calculator.py
+++ calculator.py
@@ -5,19 +5,23 @@ logging.basicConfig(level=logging.DEBUG)
 def calculator(*args):
  if action_sign == 1:
   z = float(input("Введи число 3 "))
-  return(f"Додаю {x} i {y} і {z}\nРезультат {(x + y + z)}")
+  logging.info(f"Додаю {x} i {y} і {z}\nРезультат")
+  return x + y + z
  elif action_sign == 2:
-  return(f"Віднімаю {x} i {y}\nРезультат {(x - y)}") 
+  logging.info(f"Віднімаю {x} i {y}\nРезультат") 
+  return x - y
  elif action_sign == 3:
   z = float(input("Введи число 3 "))   
-  return(f"Множу {x} i {y} і {z}\nРезультат {(x * y * z)}")
+  logging.info(f"Множу {x} i {y} і {z}\nРезультат")
+  return x * y * z
  elif action_sign == 4 and y != 0:
-  return(f"Ділю {x} i {y}\nРезультат {(x / y)}")
+  logging.info(f"Ділю {x} i {y}\nРезультат")
+  return x / y
  else:
-  return("Введено неправильний оператор")
+    logging.info("Введено неправильний оператор")
   
 if __name__ == "__main__":
  action_sign = int(input("Введи дію, використовуючи відповідне число: 1 Додавання, 2 Віднімання, 3 Множення, 4 Ділення "))
  x = float(input("Введи число 1 "))
  y = float(input("Введи число 2 "))
- print(calculator(x, action_sign, y))     
+ logging.info(calculator(x, action_sign, y))     
